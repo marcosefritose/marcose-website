8cc0c215762baa31c2a690e2fdf8b7e38a569eba
marcosefritose <m.gehrke97@t-online.de>
2022-11-26 21:42:26 +0100

    added CV download and improved navbar styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,18 @@
     export const prerender = true;
     
     import "../app.css";
+    import { page } from '$app/stores';
+
+    let path;
+    $: path = $page.url.pathname;
 </script>
 
 <div class="bg-light w-screen">
     <div class="w-full flex justify-center text-lg p-5 gap-5">
-        <a href="/">/ </a>
-        <a href="/projects">/projects </a>
-        <a href="/about">/about</a>
-        <a href="#">CV</a>
+        <a href="/" class="{path === "/" ? 'underline' : ''}">Home</a>
+        <a href="/projects" class="{path === "/projects" ? 'underline' : ''}">Projects </a>
+        <a href="/about" class="{path === "/about" ? 'underline' : ''}">About</a>
+        <a href="/CV_Marc_Gehrke.pdf" download="">CV</a>
         <a href="https://www.linkedin.com/in/marc-gehrke-a1a49a175" target="_blank" rel="noopener noreferrer"><img src="/icons/linkedin_icon.svg"></a>
         <a href="https://github.com/marcosefritose/" target="_blank" rel="noopener noreferrer"><img src="/icons/github_icon.svg"></a>
     </div>
