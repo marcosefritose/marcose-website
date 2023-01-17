8dad4d2b17c4eb50185a187ddb2f996995876714
marcosefritose <m.gehrke97@t-online.de>
2023-01-17 10:30:54 +0100

    added contact information

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,8 +1,38 @@
+<script>
+    let hide = true
+    let snippet_1 = 'hegm'
+    let snippet_2 = 'ocram@ekr'
+    let snippet_3 = 'ue.es'
+
+    function reveal() {
+        hide = false
+
+    }
+</script>
+
 <div class="relative">
     <img src="/images/marathon_marc_raw.jpg" class="border-0 w-full max-w-7xl mx-auto" alt="">
-    <div class="absolute top-48 w-full text-center">
-        <div id="textbox" class="w-3/4 lg:w-1/2 mx-auto bg-white bg-opacity-70 rounded-lg p-3">
-            <p>Coming soon.</p>
+    <div class="absolute top-48 w-full">
+        <div id="contactbox" class="bg-white bg-opacity-80 rounded-lg w-fit mx-auto py-5 px-10">
+        <h1 class="text-xl font-bold text-center mb-3">Contact</h1>
+
+            <p class="my-2">
+                <span class="reverse {hide ? 'hidden' : ''}">{snippet_3}<span class="hidden">@bot</span>{snippet_2}<span class="hidden">@stop</span>{snippet_1}</span>
+                <span class="reverse {hide ? '' : 'hidden'} cursor-pointer" on:click={reveal}>em wohs</span>
+                <span class="reverse font-semibold">:liaM</span> 
+            </p>
+            <p class="my-2">
+                <span class="font-semibold">LinkedIn:</span>
+                <a href="https://www.linkedin.com/in/marc-gehrke-a1a49a175" target="_blank" rel="noopener noreferrer">Marc Gehrke</a>
+            </p>
+            <p class="my-2">
+                <span class="font-semibold">Github</span>
+                <a href="https://github.com/marcosefritose/" target="_blank" rel="noopener noreferrer">/marcosefritose</a>
+            </p>
+            <p class="my-2">
+                <span class="font-semibold">Discord:</span>
+                <span>marcosefritose#0117</span>
+            </p>
         </div>
     </div>
 </div>
