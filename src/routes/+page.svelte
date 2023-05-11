f66ca1bd3c95db224e507a742c771da724dd70f8
marcosefritose <m.gehrke97@t-online.de>
2023-05-11 09:11:51 +0200

    feat: Changed landing display name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,10 @@
+<div class="h-screen w-full bg-blurred bg-cover bg-center bg-no-repeat" />
 
-<div class="w-full h-screen bg-blurred bg-cover bg-center bg-no-repeat">
-</div>
-
-<div class="absolute top-0 bg-light bg-opacity-60 w-full h-screen flex justify-center items-center">
-    <div class="flex flex-col pt-12">
-        <h1 class="inline-block text-5xl md:text-7xl lg:text-8xl">marcose<span class="animate-pulse cursor-pointer">.</span></h1>
-        <p class="inline-block text-lg md:text-xl">data scientist // web developer</p>
-
-    </div>
+<div class="absolute top-0 flex h-screen w-full items-center justify-center bg-light bg-opacity-60">
+	<div class="flex flex-col pt-12">
+		<h1 class="inline-block text-5xl md:text-7xl lg:text-8xl">
+			gehrke<span class="animate-pulse cursor-pointer">.</span>tech
+		</h1>
+		<p class="inline-block text-center text-lg md:text-xl">data scientist // web developer</p>
+	</div>
 </div>
